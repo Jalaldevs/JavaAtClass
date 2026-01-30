b7963f64781ed94f79a505c3ccebaead2461d169
Jalaldevs <VoiceofJalal@gmail.com>
2026-01-30 08:36:59 +0100

    Refactor MainImpresora to improve output and clarity

--- JAVA EN CLASE/COMPOSICION Y ENCAPSULACION/tarea 2 - ej - 3/MainImpresora.java
+++ JAVA EN CLASE/COMPOSICION Y ENCAPSULACION/tarea 2 - ej - 3/MainImpresora.java
@@ -1,8 +1,11 @@
 public class MainImpresora {
     public static void main(String[] args) {
-        Impresora MiNuevaImpresora = new Impresora(true);
-        MiNuevaImpresora.anadirToner(50);
-        MiNuevaImpresora.paginasAImprimir(4, false);
-        System.out.println(MiNuevaImpresora.getPaginasImpresas() + " paginas impresas con esta impresora");
+         Impresora impresora = new Impresora(true); // con doble cara
+
+        System.out.println("Nivel de toner: " + impresora.anadirToner(50));
+        System.out.println("Páginas físicas impresas: " + impresora.paginasAImprimir(5));
+        System.out.println("Total páginas impresas: " + impresora.getPaginasImpresas());
+
+        System.out.println("Nivel de toner actual: " + impresora.getNivelToner());
     }
 }
