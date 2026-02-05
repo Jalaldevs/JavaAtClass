01979403932ac3ab14ca4a99ea95dffe8a00dc61
Jalaldevs <VoiceofJalal@gmail.com>
2026-02-05 08:51:55 +0100

    Add files via upload

---
+++ JAVA EN CLASE/ABSTRACCION/Main.java
@@ -0,0 +1,12 @@
+package ABSTRACCION;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SubClase sb = new SubClase();
+        System.out.println(sb.abstract_method());
+        System.out.println(sb.nonAbstractMethod());
+    }
+
+}
