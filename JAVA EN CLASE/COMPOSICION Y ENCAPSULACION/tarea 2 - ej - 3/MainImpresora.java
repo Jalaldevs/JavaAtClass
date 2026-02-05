da1ea0ffcd762ff08cdd2afc8f06ebeb5449f6cf
Jalaldevs <VoiceofJalal@gmail.com>
2026-02-05 09:05:18 +0100

    Delete JAVA EN CLASE directory

--- JAVA EN CLASE/COMPOSICION Y ENCAPSULACION/tarea 2 - ej - 3/MainImpresora.java
+++
@@ -1,8 +0,0 @@
-public class MainImpresora {
-    public static void main(String[] args) {
-        Impresora MiNuevaImpresora = new Impresora(true);
-        MiNuevaImpresora.anadirToner(50);
-        MiNuevaImpresora.paginasAImprimir(4, false);
-        System.out.println(MiNuevaImpresora.getPaginasImpresas() + " paginas impresas con esta impresora");
-    }
-}
--- JAVA EN CLASE/INTERFACES/tarea4ej4/Regalo.java
+++
@@ -1,7 +0,0 @@
-package INTERFACES.tarea4ej4;
-
-public interface Regalo {
-
-    String RegaloPorCompra();
-
-}
