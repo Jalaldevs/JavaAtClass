9bc47a200e295dd96c82934e240780589518e0a3
Jalaldevs <VoiceofJalal@gmail.com>
2026-02-05 09:06:52 +0100

    Delete JavaAtClass/JAVA EN CLASE directory

--- JavaAtClass/JAVA EN CLASE/INTERFACES/Tarea4ej1/interfacesdeej1.java
+++
@@ -1,8 +0,0 @@
-package INTERFACES.Tarea4ej1;
-
-public interface interfacesdeej1 {
-    double methodB();        // público y abstract por defecto
-    int aConstant = 32;      // público, estático y final por defecto
-    double pi = 3.14159;     // público, estático y final por defecto
-    void methodA(int x);     // público y abstract por defecto
-}
