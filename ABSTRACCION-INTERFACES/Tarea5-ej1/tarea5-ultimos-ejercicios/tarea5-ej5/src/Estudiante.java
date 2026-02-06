c54a724d2aa57d39cf3d9ae4316fed61e0e27877
Jalaldevs <VoiceofJalal@gmail.com>
2026-02-06 09:52:13 +0100

    Delete ABSTRACCION-INTERFACES/Tarea5-ej1/tarea5-ultimos-ejercicios directory

--- ABSTRACCION-INTERFACES/Tarea5-ej1/tarea5-ultimos-ejercicios/tarea5-ej5/src/Estudiante.java
+++
@@ -1,8 +0,0 @@
-public class Estudiante implements Comparable<Estudiante>{
-    
-
-    @Override
-    public int compareTo(Estudiante o) {
-        return 0;
-    }
-}
--- ABSTRACCION-INTERFACES/Tarea5-ej1/tarea5-ultimos-ejercicios/tarea5-ej5/src/Redimensionable.java
+++
@@ -1,5 +0,0 @@
-public interface Redimensionable {
-
-    void redimensionar(int x);
-
-}
